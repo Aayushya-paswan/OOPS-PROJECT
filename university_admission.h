0e973f580cca2596a195a08c6fb574cda497b06b
Aayushya Paswan <aayushya894@gmail.com>
2025-10-27 23:00:53 +0530

    Update university_admission.h

--- university_admission.h
+++ university_admission.h
@@ -1,1 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+class university{
+    private:
+        int closing, opening;
+    public:
+        university(string, int, map<string, int>, int, int);
+        bool can_admit(Student&);
+        ~university();
+};
+
+class Student{
+    private:
+        string name;
+        int jee_rank;
+        int income;
+        vector<string> preferred_branch;
+        string contact_number;
+        int percentage_12th;
+        int age;
+        string gender;
+        string category;
+    public:
+        Student(string, int, int, vector<string>, string, int, int, string, string);
+        bool is_eligible(int, int);
+        void assign_roll();
+        void print_details();
+        bool scholarship_eligible(int, string);
+        ~Student();
+};
